5732ba23f6aa7a0be4c88b8dede6361b9252ac27
ramon <reimon1972@gmail.com>
2023-06-05 15:19:24 +0800

    add colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,40 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#003858',
+          text: '#FFFFFF',
+          lightest: '#788CA4',
+        },
+        secondary: {
+          DEFAULT: '#F58220',
+          text: '#FFFFFF',
+        },
+        text: {
+          DEFAULT: '#424242',
+          lightest: '#DCDCDC',
+          primary: '#454545',
+        },
+        muted: {
+          DEFAULT: '#7B7B7B',
+          light: '#DDDDDD',
+          lightest: '#EFF0EF',
+        },
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /(bg|text|border)-(rose|primary|secondary|muted|text|border)-(lightest)/,
+      variants: ['hover'],
+    },
+    {
+      pattern: /(bg|text|border)-chart(One|Two|Three|Four|Five|Six|Seven|Eight|Nine|Ten|Eleven)/,
+      variants: ['hover'],
+    },
+  ],
 }
 
