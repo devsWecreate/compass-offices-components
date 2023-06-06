03490cb9748dc8d72c9ff6f913b66add0ee53ab5
ramon <reimon1972@gmail.com>
2023-06-06 12:51:25 +0800

    remove command line two

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './src',
+  publicDir: 'public',
+  build: {
+    outDir: 'storybook-static',
+  },
+  server: {
+    open: true,
+  }
+
 })
